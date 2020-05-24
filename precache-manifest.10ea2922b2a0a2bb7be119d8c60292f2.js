ee65ef56f351691911566d47a502607702156b67
Denze1l <dvornikovd.o.1998@gmail.com>
2020-05-24 16:35:12 +0300

    Updates

---
+++ precache-manifest.10ea2922b2a0a2bb7be119d8c60292f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea5ae0133a368751ea5d7e26a6f8c30",
+    "url": "/norris-jokes/index.html"
+  },
+  {
+    "revision": "220b87abfdd8b6e8cc7e",
+    "url": "/norris-jokes/static/css/main.2ac092ae.chunk.css"
+  },
+  {
+    "revision": "2d9bf0949f8fb899bb7d",
+    "url": "/norris-jokes/static/js/2.ae4f643e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/norris-jokes/static/js/2.ae4f643e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220b87abfdd8b6e8cc7e",
+    "url": "/norris-jokes/static/js/main.c7f21fe9.chunk.js"
+  },
+  {
+    "revision": "cd871a4c3d7e7ef839bb",
+    "url": "/norris-jokes/static/js/runtime-main.3711954b.js"
+  },
+  {
+    "revision": "ec6223055097995a57e4bc7153d946dd",
+    "url": "/norris-jokes/static/media/heart.ec622305.svg"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/norris-jokes/static/media/like.e87899ae.svg"
+  }
+]);
